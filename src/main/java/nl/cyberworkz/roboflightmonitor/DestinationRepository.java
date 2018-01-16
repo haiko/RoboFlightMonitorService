758b8d5b460a39ed366c87d243348d613db73369
Haiko <hvdschaaf@gmail.com>
2018-01-16 18:02:59 +0100

    check for empty iataCodes

--- src/main/java/nl/cyberworkz/roboflightmonitor/DestinationRepository.java
+++ src/main/java/nl/cyberworkz/roboflightmonitor/DestinationRepository.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
@@ -36,15 +37,19 @@ public class DestinationRepository {
 	
 	
 	public Destination getDestination(String iataCode) {
-		DynamoDB db = new DynamoDB(dynamoDBClient);
 		
-		Table table =  db.getTable(destinationTable);
-		GetItemSpec spec = new GetItemSpec().withPrimaryKey("iata", iataCode);
-		
-		Item outcome = table.getItem(spec);
-		if(outcome != null) {
-			return Destination.create(outcome.getString("iata"), outcome.getString("city"), outcome.getString("country"));
+		if(StringUtils.hasText(iataCode)) {
+			DynamoDB db = new DynamoDB(dynamoDBClient);
+			
+			Table table =  db.getTable(destinationTable);
+			GetItemSpec spec = new GetItemSpec().withPrimaryKey("iata", iataCode);
+			
+			Item outcome = table.getItem(spec);
+			if(outcome != null) {
+				return Destination.create(outcome.getString("iata"), outcome.getString("city"), outcome.getString("country"));
+			}
 		}
+		
 		return new Destination();
 	}
 
