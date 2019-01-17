4f89180616727cf1ea91386d0e5f0bd7524e564a
Haiko <hvdschaaf@gmail.com>
2019-01-17 14:39:16 +0100

    change how CORS is handled

--- src/main/java/nl/cyberworkz/roboflightmonitor/LoggingInterceptor.java
+++ src/main/java/nl/cyberworkz/roboflightmonitor/LoggingInterceptor.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
 
 public class LoggingInterceptor implements HandlerInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
@@ -15,12 +16,17 @@ public class LoggingInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest req, HttpServletResponse response, Object handler)
             throws Exception {
         logger.info("adr:{}; req:\"{} {} {}\";", req.getRemoteAddr(), req.getMethod(), req.getRequestURL(), req.getProtocol());
+
         return true;
     }
 
     @Override
     public void postHandle(	HttpServletRequest request, HttpServletResponse response,
                                Object handler, ModelAndView modelAndView) throws Exception {
+        Collection<String> responseHeaders =  response.getHeaderNames();
+        for(String name: responseHeaders){
+            logger.info("header:" + name + ":" + response.getHeader(name));
+        }
     }
 
     @Override
--- src/main/java/nl/cyberworkz/roboflightmonitor/RoboFlightMonitorController.java
+++ src/main/java/nl/cyberworkz/roboflightmonitor/RoboFlightMonitorController.java
@@ -11,11 +11,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -24,6 +20,8 @@ import nl.cyberworkz.roboflightmonitor.domain.Flight;
 import nl.cyberworkz.roboflightmonitor.domain.FlightResponse;
 import nl.cyberworkz.roboflightmonitor.exceptions.BadRequestException;
 import nl.cyberworkz.roboflightmonitor.exceptions.NotFoundException;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Handles incoming requests
@@ -43,9 +41,8 @@ public class RoboFlightMonitorController {
 	private RoboFlightMonitorService service;
 
 	/**
-	 * 
 	 * Get Flights
-	 * 
+	 *
 	 * @param page
 	 * @return
 	 * @throws JsonParseException
@@ -53,39 +50,46 @@ public class RoboFlightMonitorController {
 	 * @throws BadRequestException
 	 * @throws IOException
 	 */
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<FlightResponse> getCurrentArrivingFlights(@RequestParam(defaultValue = "0") int page, @RequestParam(required=false) String scheduleTime)
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<FlightResponse> getCurrentArrivingFlights(@RequestParam(defaultValue = "0") int page, @RequestParam(required = false) String scheduleTime)
 			throws JsonParseException, JsonMappingException, BadRequestException, IOException {
 
 		if (page > 30) {
 			throw new BadRequestException("page param is invalid!");
 		}
-		
+
 		FlightResponse flightResponse = null;
-		
-		if(scheduleTime ==null) {
+
+		if (scheduleTime == null) {
 			flightResponse = service.getArrivingFlights(page);
-		}
-		else {
+		} else {
 			DateTime time = DateTime.parse(scheduleTime, DateTimeFormat.forPattern("HH:mm"));
 			flightResponse = service.getArrivingFlights(page, time);
 		}
-		
+
 		return new ResponseEntity<FlightResponse>(flightResponse, HttpStatus.OK);
 	}
 
 	/**
 	 * Get a specific {@link Flight} for given id.
-	 * 
+	 *
 	 * @param flightId
 	 * @return
 	 * @throws NotFoundException
-	 * @throws IOException 
-	 * @throws JsonMappingException 
-	 * @throws JsonParseException 
+	 * @throws IOException
+	 * @throws JsonMappingException
+	 * @throws JsonParseException
 	 */
-	@RequestMapping(method = RequestMethod.GET, value = "/{flightId}")
+	@RequestMapping(method = RequestMethod.GET, value = "/{flightId}", produces = "application/json")
 	public ResponseEntity<Flight> getFlight(@PathVariable String flightId) throws NotFoundException, JsonParseException, JsonMappingException, IOException {
 		return new ResponseEntity<Flight>(service.getFlight(flightId), HttpStatus.OK);
+	}
+
+	@RequestMapping(value= "/**", method=RequestMethod.OPTIONS)
+	public void corsHeaders(HttpServletResponse response) {
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+		response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
+		response.addHeader("Access-Control-Max-Age", "3600");
 	}
 }
